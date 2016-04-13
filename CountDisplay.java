fc49e3c27af1bf046f496cfd9a9c454bea36e5f2
CamdenFischer <cwf218@lehigh.edu>
2016-04-12 21:23:03 -0400

    Added files via upload

---
+++ CountDisplay.java
@@ -0,0 +1,89 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+// -------------------------------------------------------------------------
+/**
+ * Write a one-sentence summary of your class here. Follow it with additional
+ * details about its purpose, what abstraction it represents, and how to use it.
+ *
+ * @author Camden Fischer
+ * @version Apr 11, 2016
+ */
+public class CountDisplay
+    implements IHuffModel
+{
+    /**
+     * Creating a CharCounter
+     */
+    ICharCounter        cc = new CharCounter();
+    /**
+     * Creating a BitInputStream
+     */
+    private InputStream bits;
+
+
+    public void initialize(InputStream stream)
+    {
+        try
+        {
+            bits = new BitInputStream(
+                new FileInputStream(
+                    "X:\\Documents\\Spring 2016\\CSE 017\\text1.txt"));
+        }
+        catch (FileNotFoundException e)
+        {
+
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void showCounts()
+    {
+
+        try
+        {
+            cc.countAll(bits);
+        }
+        catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 256; i++)
+        {
+            if (cc.getCount(i) != 0)
+            {
+                System.out.println((char)i + ": " + cc.getCount(i));
+            }
+        }
+
+    }
+
+
+    public void showCodings()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    public void write(InputStream stream, File file, boolean force)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    public void uncompress(InputStream in, OutputStream out)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
