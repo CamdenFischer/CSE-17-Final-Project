aa1710b584454855e78e843a5fb0575f765404e7
CamdenFischer <cwf218@lehigh.edu>
2016-04-25 16:46:03 -0400

    Added files via upload

--- CountDisplay.java
+++ CountDisplay.java
@@ -25,14 +25,18 @@ public class CountDisplay
      */
     private InputStream bits;
 
+    static MinHeap      Hheap;
+
+    private int[]       coding;
+
+    HuffTree tree;
+
 
     public void initialize(InputStream stream)
     {
         try
         {
-            bits = new BitInputStream(
-                new FileInputStream(
-                    "X:\\Documents\\Spring 2016\\CSE 017\\text1.txt"));
+            bits = new BitInputStream(new FileInputStream("test.txt"));
         }
         catch (FileNotFoundException e)
         {
@@ -48,6 +52,7 @@ public class CountDisplay
 
         try
         {
+            bits = new BitInputStream(new FileInputStream("test.txt"));
             cc.countAll(bits);
         }
         catch (IOException e)
@@ -72,6 +77,7 @@ public class CountDisplay
 
         try
         {
+            bits = new BitInputStream(new FileInputStream("test.txt"));
             cc.countAll(bits);
         }
         catch (IOException e)
@@ -87,29 +93,112 @@ public class CountDisplay
                 count++;
             }
         }
-
         HuffTree[] treeArr = new HuffTree[count];
         int x = 0;
-        for (int i = 0; i < 256; i++)   //creating an array of trees that have
-                                        //non zero counts
+        for (int i = 0; i < 256; i++) // creating an array of trees that have
+                                      // non zero counts
         {
             if (cc.getCount(i) != 0)
             {
-                treeArr[x] = new HuffTree((char) i, cc.getCount(i));
+                treeArr[x] = new HuffTree((char)i, cc.getCount(i));
+                x++;
             }
         }
 
-        MinHeap heap = new MinHeap(treeArr, count, 255);
+        Hheap = new MinHeap(treeArr, count, 256);
+        tree = buildTree(Hheap);
+        coding = new int[count];
+
+        traversal(tree.root(), coding, 0);
 
     }
 
 
+    // ----------------------------------------------------------
+    /**
+     * create a method called traversal to encode
+     *
+     * @param node
+     *            is a HuffBaseNode type
+     * @param codes
+     *            is an integer array type
+     * @param top
+     *            is an integer type
+     */
+    public void traversal(HuffBaseNode node, int[] codes, int top)
+    {
+        if (((HuffInternalNode)node).left() != null)
+        {
+            codes[top] = 0;
+            traversal(((HuffInternalNode)node).left(), codes, top + 1);
+        }
+        if (((HuffInternalNode)node).right() != null)
+        {
+            codes[top] = 1;
+            traversal(((HuffInternalNode)node).right(), codes, top + 1);
+        }
+        if (node.isLeaf())
+        {
+            System.out.printf("%c: ", ((HuffLeafNode)node).element());
+            printArray(codes, top);
+        }
+
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * create a method called printArray to print the encoding
+     *
+     * @param array
+     *            is an integer array
+     * @param n
+     *            is an integer
+     */
+    public void printArray(int array[], int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            System.out.printf("%d", array[i]);
+            System.out.println();
+        }
+    }
 
 
     public void write(InputStream stream, File file, boolean force)
     {
-        // TODO Auto-generated method stub
+        BitOutputStream out = new BitOutputStream(
+            "X:\\Documents\\Spring 2016\\CSE 017\\text1.txt");
+        out.write(BITS_PER_INT, MAGIC_NUMBER);
+        traverse(tree.root(), out);
+        out.write(1, 1);
+        out.write(9, PSEUDO_EOF);
 
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Preorder traversal that writes 1s and 0s as needed. part 3 step 2
+     * @param root of hufftree
+     * @param out output stream
+     */
+    public void traverse(HuffBaseNode root, BitOutputStream out)
+    {
+        if (root == null)
+        {
+            return;
+        }
+        if (!root.isLeaf())
+        {
+            out.write(1, 0);
+        }
+        else
+        {
+            out.write(1, 1);
+            out.write(9, ((HuffLeafNode)root).element());
+        }
+        traverse(((HuffInternalNode)root).left(), out);
     }
 
 
@@ -117,6 +206,24 @@ public class CountDisplay
     {
         // TODO Auto-generated method stub
 
+    }
+
+
+    static HuffTree buildTree(MinHeap Hheap)
+    {
+        HuffTree tmp1, tmp2, tmp3 = null;
+
+        while (Hheap.heapsize() > 1)
+        { // While two items left
+            tmp1 = (HuffTree)Hheap.removemin();
+            tmp2 = (HuffTree)Hheap.removemin();
+            tmp3 = new HuffTree(
+                tmp1.root(),
+                tmp2.root(),
+                tmp1.weight() + tmp2.weight());
+            Hheap.insert(tmp3); // Return new tree to heap
+        }
+        return tmp3; // Return the tree
     }
 
 }
